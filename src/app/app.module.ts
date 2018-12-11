1fbb57f8aa57431ae0d4aa931d9f1f38fd438dec
bbaral <bikramb1@asu.edu>
2018-12-10 20:22:18 -0500

    restoring most of the file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DetailsComponent,
   ],
   imports: [
     BrowserModule,
