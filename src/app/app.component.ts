d8499fc27a3a8d5bf332b81cd55b5581ceabc5fe
bbaral <bikramb1@asu.edu>
2018-12-10 20:59:39 -0500

    component and data-binding -- deep dive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  serverElements = [];
+  serverElements = [{type: 'server', name: 'TestServer', content: 'Just a test!'}];
 }
