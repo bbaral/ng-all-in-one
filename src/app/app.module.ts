824169a51e10c128828aa4ab7fbef0641d81dfc3
bbaral <bikramb1@asu.edu>
2018-12-13 22:01:36 -0500

    service and DI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DetailsComponent,
   ],
   imports: [
     BrowserModule,
