ed73a90fece088a4e9ba6a4a48f893423c7fdceb
Bikram Baral <bbaral@users.noreply.github.com>
2018-12-17 16:21:55 -0500

    Update users.component.ts

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -7,6 +8,7 @@ import { Component } from '@angular/core';
 })
 export class UsersComponent {
 
+ constructor(private router: Router) {}
   users = [
     {
       id: 1,
