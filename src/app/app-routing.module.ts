4c9427f3ca6c93b9a7abcb5b5bb4a872d446791d
bbaral <bikramb1@asu.edu>
2018-12-16 22:12:02 -0500

    RouterLink and RouterLinkActiveOptions to enable the navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {UserComponent} from './users/user/user.component';
+import {HomeComponent} from './home/home.component';
+import {ServersComponent} from './servers/servers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'users', component: UserComponent},
+  {path: 'servers', component: ServersComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
