26391aa84c8172e385edfec165e6ee0154c16e56
bbaral <bikramb1@asu.edu>
2018-12-16 19:44:17 -0500

    Router practice initialize

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  users = [
+    {
+      id: 1,
+      name: 'Max'
+    },
+    {
+      id: 2,
+      name: 'Anna'
+    },
+    {
+      id: 3,
+      name: 'Chris'
+    }
+  ];
 }
