0bde6a5f583a9ba6ab2e837178e91e72b106dc6e
bbaral <bikramb1@asu.edu>
2018-12-16 19:40:26 -0500

    Router practice initialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DetailsComponent,
+    HomeComponent,
+    ServersComponent,
+    EditServerComponent,
+    ServerComponent,
   ],
   imports: [
     BrowserModule,
