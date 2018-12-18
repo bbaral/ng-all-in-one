bbdd777463c8de3fb1de98a08f2df718398480ee
Bikram Baral <bbaral@users.noreply.github.com>
2018-12-18 16:26:13 -0500

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {interval, Observable} from 'rxjs';
+import {Observer} from 'rxjs/Observer';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  number: number = 0;
+  someValue: any;
+  subscription$: Subscription;
+  anotherSubscription$: Subscription;
+  constructor(private cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
+    const myNumber = interval(1000);
+
+    this.subscription$ = myNumber.subscribe((val: number) => {
+      this.number = val;
+      console.log(this.number);
+    });
+
+    const hello = Observable.create((observer: Observer<any>) => {
+      setTimeout(() => {
+        observer.next('hello World');
+      }, 800);
+
+      setTimeout(() => {
+        observer.next('hello Planet');
+      }, 400);
+    });
+
+    this.anotherSubscription$ = hello.subscribe((val: any) => {
+      this.someValue = val;
+    });
+
+  }
+
+  TimerButtonStop() {
+    this.cdr.detach();
+  }
+
+  TimerButtonStart() {
+    this.cdr.reattach();
+  }
+
+  TimerButtonCheck() {
+    this.cdr.detectChanges();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription$.unsubscribe();
+    this.anotherSubscription$.unsubscribe();
+    console.log('ngOnDestroy call');
   }
 
 }
+
