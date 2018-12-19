d57a2c9599aae7a69014a5cc66acd66ba44b8046
Bikram Baral <bbaral@users.noreply.github.com>
2018-12-19 13:06:05 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UsersService} from './services/users.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  username = '';
-  is_editable = false;
+export class AppComponent implements OnInit, OnDestroy {
+  user1Activated: boolean = false;
+  user2Activated: boolean = false;
+  subscription: Subscription;
 
-  buttonClicked() {
-    this.username = 'Yay I am clicked!';
+  constructor(private userService: UsersService) {}
+
+  ngOnInit(): void {
+    this.subscription = this.userService.userActivated.subscribe((id: number) => {
+      if (id === 1) {
+        this.user1Activated = true;
+      }
+      if (id === 2) {
+        this.user2Activated = true;
+      }
+    });
   }
 
-  isDisabled() {
-    if (this.username.length > 10) {
-     return this.is_editable = true;
-    } else {
-      return this.is_editable;
-    }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
