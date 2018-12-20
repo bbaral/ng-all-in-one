9c3d8934150eedb89c6315161e9438ef14be1396
Bikram Baral <bikramb1@asu.edu>
2018-12-19 20:10:06 -0500

    Promise to resolve the and reject the navigation of route

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,18 +1,25 @@
-import {ActivatedRouteSnapshot,
-  CanActivate,
-  RouterStateSnapshot} from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate, Router,
+  RouterStateSnapshot
+} from '@angular/router';
 import {Observable} from 'rxjs';
 import {Injectable} from '@angular/core';
 import {AuthService} from './auth.service';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
-  constructor(private authService: AuthService) {}
+
+  constructor(private authService: AuthService,
+              private router: Router) {}
+
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    this.authService.isAuthenticated().then((authenticated: boolean) => {
-
+   return this.authService.isAuthenticated().then((authenticated: boolean) => {
+      if (authenticated) {
+        return true;
+      } else {
+        this.router.navigate(['/']);
+      }
     });
-
-    return true;
   }
 }
