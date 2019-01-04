a068bc5b4f381a2af0e9238c0c025dd461584de5
Bikram Baral <bikramb1@asu.edu>
2019-01-03 22:06:49 -0500

    ng bootstrap alert pop up component added

---
+++ src/app/shared/popup-window/popup-window.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {ModalDismissReasons, NgbActiveModal, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-popup-window',
+  templateUrl: './popup-window.component.html',
+  styleUrls: ['./popup-window.component.scss']
+})
+export class PopupWindowComponent {
+  constructor(private modalService: NgbModal) { }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      console.log(result);
+    }, (reason) => {
+      console.log(reason);
+    });
+  }
+}
