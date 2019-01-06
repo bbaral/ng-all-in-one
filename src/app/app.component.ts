0c8115b67919afe370fecb5e47e3aa776821df59
Bikram Baral <bikramb1@asu.edu>
2019-01-06 18:36:03 -0500

    two way data binding as well ngModel only

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,28 @@ import {FormGroup, NgForm} from '@angular/forms';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  @ViewChild('f', {read: NgForm}) f: NgForm;
+  @ViewChild('f', {read: NgForm}) signUpForm: NgForm;
   defaultQuestion: string = 'teacher';
   defaultUsername: string = 'bbaral';
   defaultEmail: string = 'test@test.com';
   answer: string = '';
   genders: any = ['Male', 'Female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted: boolean = false;
 
   constructor() {}
 
   suggestUserName() {
     const suggestedName = 'Superuser';
-    this.f.setValue({
+    //Another way of setValue in form in through patch.
+    //which is available only in form
+    /**this.f.setValue({
       userData: {
         username: suggestedName,
         email: ''
@@ -27,9 +37,22 @@ export class AppComponent {
       questionAnswer: 'Pre populating via SuggestedUserName Button',
       gender: 'Male'
     });
+     */
+
+    this.signUpForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   onSubmit(): void {
-    console.log(this.f);
+    this.submitted = true;
+    console.log(this.signUpForm);
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.email = this.signUpForm.value.userData.email;
+    this.user.secretQuestion = this.signUpForm.value.secret;
+    this.user.answer = this.signUpForm.value.questionAnswer;
+    this.user.gender = this.signUpForm.value.gender;
   }
 }
