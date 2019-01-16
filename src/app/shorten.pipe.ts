825e87009b7ba574b7d62eff6915885a540fff0c
Bikram Baral <bikramb1@asu.edu>
2019-01-15 20:13:01 -0500

    custom pipe

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -6,8 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ShortenPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    if (value.length > 10) {
-      return value.substr(0, 10) + '...';
+    if (value.length > args) {
+      return value.substr(0, args) + '...';
     } else {
       return value;
     }
