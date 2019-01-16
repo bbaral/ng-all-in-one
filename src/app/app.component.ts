aae998d842db9d8f4bca61afd3ffc231d9cde2a6
Bikram Baral <bikramb1@asu.edu>
2019-01-15 21:57:19 -0500

    Http request and response practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import {Component, TemplateRef, ViewChild} from '@angular/core';
-import {FormGroup, NgForm} from '@angular/forms';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,73 +6,27 @@ import {FormGroup, NgForm} from '@angular/forms';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  filteredStatus: string = '';
-
-  appStatus = new Promise((resolve, reject) => {
-   setTimeout(() => {
-     resolve('stable');
-   }, 0);
-  });
-
   servers = [
     {
-      instanceType: 'medium',
-      name: 'Production Server',
-      status: 'stable',
-      started: new Date(10, 0, 2017)
+      name: 'Testserver',
+      capacity: 10,
+      id: this.generateId()
     },
     {
-      instanceType: 'large',
-      name: 'User',
-      status: 'stable',
-      started: new Date(15, 1, 2017)
-    },
-    {
-      instanceType: 'large',
-      name: 'DB Admin',
-      status: 'critical',
-      started: new Date(15, 1, 2010)
-    },
-    {
-      instanceType: 'small',
-      name: 'Development Server',
-      status: 'offline',
-      started: new Date(15, 1, 2017)
-    },
-    {
-      instanceType: 'small',
-      name: 'Testing Environment Server',
-      status: 'stable',
-      started: new Date(15, 1, 2017)
-    },
-    {
-      instanceType: 'Severe',
-      name: 'System Analyst',
-      status: 'critical',
-      started: new Date(19, 12, 1995)
-    },
-    {
-      instanceType: 'Large',
-      name: 'DevOps Engineer',
-      status: 'critical',
-      started: new Date(30, 5, 2010)
-    },
+      name: 'Liveserver',
+      capacity: 100,
+      id: this.generateId()
+    }
   ];
-
-  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
-    return {
-      'list-group-item-success': server.status === 'stable',
-      'list-group-item-warning': server.status === 'offline',
-      'list-group-item-danger': server.status === 'critical'
-    };
-  }
-
-  onAddServer() {
+  onAddServer(name: string) {
     this.servers.push({
-      instanceType: 'small',
-      name: 'New Server',
-      status: 'stable',
-      started: new Date(Date.now())
+      name: name,
+      capacity: 50,
+      id: this.generateId()
     });
   }
+  private generateId() {
+    return Math.round(Math.random() * 10000);
+  }
+
 }
