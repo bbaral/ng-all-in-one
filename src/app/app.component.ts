d40b59ee46cc58815388f57e9d123bce325701dc
Bikram Baral <bikramb1@asu.edu>
2019-05-10 00:05:24 -0400

    storing old value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, ElementRef, OnInit, TemplateRef, ViewChild, ViewRef} from '@angular/core';
 import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Observable} from 'rxjs';
+import {store} from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,8 @@ import {Observable} from 'rxjs';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  data = [];
+  temp = '';
   genders = ['Male', 'Female'];
   signUpForm: FormGroup;
   forbiddenUsername = ['mia', 'jim'];
@@ -22,19 +25,30 @@ export class AppComponent implements OnInit {
         'email': new FormControl(null,
           [Validators.required, Validators.email], this.forbiddenEmails.bind(this))
       }),
+      'id': new FormControl('Bikram', [Validators.required]),
       'gender': new FormControl('Male', Validators.required),
       'hobbies': new FormArray([])
     });
-    this.signUpForm
+/*    this.signUpForm
       .controls['username']
       .valueChanges.subscribe((value) => {
       console.log('Old username: ', this.signUpForm.value['username']);
       console.log('new username: ', value);
-    });
+    });*/
+  }
+
+  store(newValue: string) {
+    this.data.push(newValue);
+    console.log(this.data);
   }
 
   onSubmit() {
     console.log(this.signUpForm);
+    if (this.data[0] !== this.signUpForm.get('id').value) {
+      this.temp = this.data[1];
+      console.log(this.temp);
+      alert('Bingo!!');
+    }
   }
 
   onAddHobby() {
