017bc5b5ca52700de7ac1cf9a44bfc36e1ba1e4d
bbaral <baralbikram@hotmail.com>
2019-12-25 14:11:44 -0500

    queryParams

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -23,14 +23,14 @@ export class ServerComponent implements OnInit {
           this.server = data['server'];
         }
       );
-    // const id = +this.route.snapshot.params['id'];
-    // this.server = this.serversService.getServer(id);
-    // this.route.params
-    //   .subscribe(
-    //     (params: Params) => {
-    //       this.server = this.serversService.getServer(+params['id']);
-    //     }
-    //   );
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.server = this.serversService.getServer(+params['id']);
+        }
+      );
   }
 
   onEdit() {
