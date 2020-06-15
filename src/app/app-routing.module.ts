ec46c4a59e441e39c0b259c694709ba92495303f
bbaral <baralbikram@hotmail.com>
2020-06-14 23:14:40 -0400

    angular dev upgrade v9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import {HomeComponent} from './core/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  {path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
+  {path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule)},
   { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
